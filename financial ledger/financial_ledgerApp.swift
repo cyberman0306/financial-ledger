518c4ae585a849eed996ac13491bfc630842a087
HYUN JUN YOOK <yhj03069@gmail.com>
2023-01-31 09:37:43 +0900

    Initial Commit

---
+++ financial ledger/financial_ledgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  financial_ledgerApp.swift
+//  financial ledger
+//
+//  Created by cnu on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct financial_ledgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
